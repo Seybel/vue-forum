98983018c1ed2e8356502fd614d99e010a5dee76
Seybel <seyibello31@gmail.com>
2021-03-27 14:47:53 +0100

    updated stylesheet

--- src/components/PageHome.vue
+++ src/components/PageHome.vue
@@ -1,5 +1,12 @@
 <template>
   <div>This is the home page</div>
+  <div v-for="thread in threads" :key="thread.id">
+      <h2>{{ thread.title }}</h2>
+      <div v-for="postId in thread.posts" :key="postId">
+          <p><b>{{ userById(postById(postId).userId).name }}</b></p>
+          <p>{{ postById(postId).text }}</p>
+      </div>
+  </div>
 </template>
 
 <script>
@@ -9,7 +16,17 @@ console.log(sourceData)
 export default {
   data () {
     return {
-      sourceData: sourceData
+      threads: sourceData.threads,
+      posts: sourceData.posts,
+      users: sourceData.users
+    }
+  },
+  methods: {
+    userById (userId) {
+        return this.users.find(u => u.id === userId)
+    },
+    postById (postId) {
+        return this.posts.find(p => p.id === postId)
     }
   }
 }
