334325477537a09c8f392e1566babdb6a9ed7589
Seybel <seyibello31@gmail.com>
2021-03-27 12:59:16 +0100

    init

---
+++ src/components/PageHome.vue
@@ -0,0 +1,20 @@
+<template>
+  <div>This is the home page</div>
+</template>
+
+<script>
+import sourceData from '@/data.json'
+console.log(sourceData)
+
+export default {
+  data () {
+    return {
+      sourceData: sourceData
+    }
+  }
+}
+</script>
+
+<style>
+
+</style>
